24e63043244a9ba29bf815b032b6433118e8ce4d
jjsskk <jinsukk9763@gmail.com>
2022-07-29 17:56:35 +0900

    9095 풀이

---
+++ 다이나믹 프로그래밍/9095.1, 2, 3 더하기.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().split("\n").map((e)=>{
+    return Number(e);
+});;
+
+const memo = [0, 1, 2, 4];
+
+function bottomUp(x) {
+    for (let i = 4; i <= x; i++) {
+        memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3];
+    }
+    return memo[x];
+}
+
+for (let i = 1; i <= input[0]; i++) {
+    console.log(bottomUp(input[i]));
+}
