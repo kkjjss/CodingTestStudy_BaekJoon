00dbb67ea499ce6f3e51a96606284e76d3bb1856
kkjjss <jinsukk9763@gmail.com>
2022-08-03 10:42:23 +0900

    11057. 오르막 수 풀이

--- 다이나믹 프로그래밍/11057.오르막수.js
+++ 다이나믹 프로그래밍/11057.오르막수.js
@@ -3,20 +3,20 @@ const input = fs.readFileSync("/dev/stdin").toString().trim();
 
 const num = Number(input);
 
-const memo = [[], [0, 1, 1, 1, 1, 1, 1, 1, 1, 1]];
+const memo = [[], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]];
 
 function bottomUp(num) {
     for (let i = 2; i <= num; i++) {
         memo[i] = [];
         for (let j = 0; j < 10; j++) {
-            memo[i][j] =
-                ((memo[i - 1][j - 1] || 0) + (memo[i - 1][j + 1] || 0)) %
-                1000000000;
+            memo[i][j] = memo[i - 1].reduce((acc, cur, idx) => {
+                return (idx <= j ? acc + cur : acc) % 10007;
+            });
         }
     }
-    return memo[num].reduce((acc, i) => {
-        return (acc + i) % 1000000000;
-    }, 0);
+    return memo[num].reduce((acc, cur) => {
+        return (acc + cur) % 10007;
+    });
 }
 
 console.log(bottomUp(num));
