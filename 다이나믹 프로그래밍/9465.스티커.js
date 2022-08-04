e901611a25c564dc1dddfa374778913f159ed4ef
jjsskk <jinsukk9763@gmail.com>
2022-08-04 19:01:43 +0900

    9455.스티커 풀이

---
+++ 다이나믹 프로그래밍/9465.스티커.js
@@ -0,0 +1,37 @@
+// const fs = require("fs");
+// const input = fs.readFileSync("/dev/stdin").toString().split('\n');
+
+const input = `2
+5
+50 10 100 20 40
+30 50 70 10 60
+7
+10 30 10 50 100 20 40
+20 40 30 50 60 20 80`
+    .toString()
+    .split("\n");
+
+const caseNum = input[0];
+
+const inputArr = [];
+
+for (let i = 0; i < caseNum; i++) {
+    inputArr[i] = { n: input[3 * i + 1], sticker: [input[3 * i + 2].split(" ").map((n) => BigInt(n)), input[3 * i + 3].split(" ").map((n) => BigInt(n))] };
+}
+
+for (let i = 0; i < caseNum; i++) {
+    console.log(bottomUp(inputArr[i]));
+}
+
+function bottomUp({ n, sticker }) {
+    const memo = [
+        [sticker[0][0], sticker[1][0] + sticker[0][1]],
+        [sticker[1][0], sticker[0][0] + sticker[1][1]],
+    ];
+
+    for (let i = 2; i < n; i++) {
+        memo[0][i] = sticker[0][i] + memo[1][i - 1] > sticker[0][i] + memo[1][i - 2] ? sticker[0][i] + memo[1][i - 1] : sticker[0][i] + memo[1][i - 2];
+        memo[1][i] = sticker[1][i] + memo[0][i - 1] > sticker[1][i] + memo[0][i - 2] ? sticker[1][i] + memo[0][i - 1] : sticker[1][i] + memo[0][i - 2];
+    }
+    return Number(memo[0][n - 1] > memo[1][n - 1] ? memo[0][n - 1] : memo[1][n - 1]);
+}
