caf7753efe59ef69fd95688b62ed3b52c08611dc
jjsskk <jinsukk9763@gmail.com>
2022-08-10 14:33:57 +0900

    11055 가장 큰 증가 부분 수열

---
+++ 다이나믹 프로그래밍/11055.가장 큰 증가 부분 수열.js
@@ -0,0 +1,22 @@
+// const fs = require("fs");
+// const input = fs.readFileSync("/dev/stdin").toString().split(/[\n ]/).map((e) => e * 1);
+const input = `3
+3 2 3`.split(/[\n ]/).map((e) => e * 1);
+
+const memo = [0, input[1]];
+let temp = input[1];
+
+function bottomUp(num) {
+    for (let i = 2; i <= num; i++) {
+        memo[i] = input[i];
+        for (let j = i - 1; j > 0; j--) {
+            if (input[j] < input[i]) {
+                if(memo[j] + input[i] >= memo[i]) memo[i] = memo[j] + input[i];
+            }
+        }
+        if (memo[i] > temp) temp = memo[i];
+    }
+    return temp;
+}
+
+console.log(bottomUp(input[0]));
